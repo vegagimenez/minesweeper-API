ff1c1cfe23ab88f0346c7990776eb44428640335
Maximiliano Vega Gimenez <mvega@belatrixsf.com>
2019-12-23 14:19:24 -0300

    Exception helper implementation to have quick exceptions for api's

---
+++ src/main/java/ar/com/jmvg/challenges/minesweeper/api/exception/MinesweeperAPIExceptionHelper.java
@@ -0,0 +1,44 @@
+package ar.com.jmvg.challenges.minesweeper.api.exception;
+
+import java.util.List;
+
+import org.apache.http.HttpStatus;
+
+/**
+ * Class to have some quick implementation to throw Exceptions in APi's process
+ * */
+public class MinesweeperAPIExceptionHelper {
+
+	public static MinesweeperAPIException forbiddenException() {
+		return new MinesweeperAPIException("You are not allowed not do this operation.", HttpStatus.SC_FORBIDDEN);
+	}
+	public static MinesweeperAPIException notFoundException() {
+		return new MinesweeperAPIException("Not found.", HttpStatus.SC_NOT_FOUND);
+	}
+	public static MinesweeperAPIException notFoundException(String data) {
+		return new MinesweeperAPIException("Entity with Id:" + data + " not found.", HttpStatus.SC_NOT_FOUND);
+	}
+	public static MinesweeperAPIException alreadyExistsException() {
+		return new MinesweeperAPIException("Entity already exists.", HttpStatus.SC_PRECONDITION_FAILED);
+	}
+	public static MinesweeperAPIException notAcceptedException() {
+		return new MinesweeperAPIException("Not accepted.", HttpStatus.SC_NOT_ACCEPTABLE);
+	}
+	public static MinesweeperAPIException invalidArgumentsException(List<String> invalidFields) {
+		return MinesweeperAPIExceptionHelper.invalidArgumentsException(invalidFields.toString());
+	}
+	public static MinesweeperAPIException invalidArgumentsException(String invalidField) {
+		StringBuffer sb = new StringBuffer("Invalid arguments exception in fields:");
+		sb.append(invalidField.toString());	
+		return new MinesweeperAPIException(sb.toString(), HttpStatus.SC_BAD_REQUEST);
+	}
+	public static MinesweeperAPIException invalidArgumentsException() {
+		return new MinesweeperAPIException("Invalid arguments.", HttpStatus.SC_BAD_REQUEST);
+	}
+	public static MinesweeperAPIException defaultException(String message, Throwable cause) {
+		return new MinesweeperAPIException(message, HttpStatus.SC_INTERNAL_SERVER_ERROR, cause);
+	}
+	public static MinesweeperAPIException internalServerErrorException() {
+		return new MinesweeperAPIException("Unexpected exception, please contact our support administration.", HttpStatus.SC_INTERNAL_SERVER_ERROR);
+	}
+}
