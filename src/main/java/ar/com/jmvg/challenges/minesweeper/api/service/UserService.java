306dea24febe628622ef56d08701ad97312a75e5
Maximiliano Vega Gimenez <mvega@belatrixsf.com>
2019-12-26 11:00:27 -0300

    Changed config to test deploy in GCP-GAE

--- src/main/java/ar/com/jmvg/challenges/minesweeper/api/service/UserService.java
+++ src/main/java/ar/com/jmvg/challenges/minesweeper/api/service/UserService.java
@@ -4,6 +4,13 @@ import org.springframework.stereotype.Service;
 
 import ar.com.jmvg.challenges.minesweeper.api.model.User;
 
+/**
+ * User service implementation, yes I can create Interfaces for the every service but I think for some projects
+ * it's better to start coding features and let the abstraction when you need to have it, before that, you could lose precious time
+ * discussing the better abstraction or anything else...anyway... is arguable.
+ * 
+ * Like Thanos said: "Perfectly balanced." (velocity/feature)
+ * */
 @Service
 public class UserService {
 
